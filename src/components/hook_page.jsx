a6dc126c1fbee8fe1c6ed0c1bb536c47f5507b7a
gautam-manish <manishgtm123@gmail.com>
2024-03-06 11:27:58 +0545

    Second Commit

---
+++ src/components/hook_page.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Hookpage() {
+    let [count, setCount] = React.useState(0)
+    let [check, setCheck] = React.useState("")
+    let countRef = React.useRef(0);
+    const handleClick = () =>{
+        setCount(count+1);
+        countRef.current = countRef.current+1;
+    }
+ 
+    const incrementCounter = () => {
+        setCount(count + 1);
+    }
+
+    const decrementCounter = () => {
+        setCount(count - 1);
+    }
+    const resetBtn = () => {
+        setCount(0);
+    }
+    const evenOdd = () => {
+        if (count % 2 == 0) {
+            setCheck("Even");
+        }
+        else {
+            setCheck("Odd");
+        }
+    }
+
+return (
+    <>
+        <div>
+            <h1>Hook Example</h1>
+            <h3>Use State Hook</h3>
+            <h6>Counter Value: {count}</h6>
+            <h6>Counter Value: {check}</h6>
+            <button onClick={incrementCounter}>Increment</button>
+            <button onClick={resetBtn}>Reset</button>
+            <button onClick={decrementCounter}>Decrement</button>
+            <button onClick={evenOdd}>Check</button>
+            <p>user ref Count: {countRef.current} </p>
+            <h3>Use State Hook</h3>
+            <button onClick={handleClick}>Increment</button>
+            <h3>Use Ref Hook</h3>
+
+        </div>
+    </>
+);
+}
+
+export default Hookpage
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hookpage from './components/hook_page';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Hookpage/>
   </React.StrictMode>
 );
 
